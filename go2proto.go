a310017722b301d646f8e9b015f37c0f98937ca0
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2025-01-28 20:59:04 -0500

    fix number generation

--- go2proto.go
+++ go2proto.go
@@ -15,7 +15,6 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/iancoleman/strcase"
 	"golang.org/x/tools/go/packages"
 )
 
@@ -321,9 +320,31 @@ func isRepeated(f *types.Var) bool {
 	return ok
 }
 
-// toProtoFieldName transforms the Go field name into snake_case for proto using strcase.
+// toProtoFieldName transforms the Go field name into snake_case for proto without treating numbers as word boundaries.
 func toProtoFieldName(name string) string {
-	return strcase.ToSnake(name)
+	var result []rune
+	for i, r := range name {
+		if i > 0 && isUpper(r) && (isLower(rune(name[i-1])) || isLower(rune(name[i+1]))) {
+			result = append(result, '_')
+		}
+		result = append(result, toLower(r))
+	}
+	return string(result)
+}
+
+func isUpper(r rune) bool {
+	return r >= 'A' && r <= 'Z'
+}
+
+func isLower(r rune) bool {
+	return r >= 'a' && r <= 'z'
+}
+
+func toLower(r rune) rune {
+	if isUpper(r) {
+		return r + ('a' - 'A')
+	}
+	return r
 }
 
 // processEnumIfAny returns the possible values of the field's type if it's recognized as an enum.
