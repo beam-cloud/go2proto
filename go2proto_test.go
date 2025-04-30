3bc5e2e7764b278f752e989daed3f93005d777f8
John <john@slai.io>
2025-04-30 13:02:20 -0700

    Ignore field for go2proto gen

--- go2proto_test.go
+++ go2proto_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,5 +32,29 @@ func TestGetMessages(t *testing.T) {
 
 	for _, enum := range enums {
 		t.Logf("enum: %s", enum.Name)
+	}
+}
+
+func TestIgnoredField(t *testing.T) {
+	pkgs, err := loadPackages(".", []string{"./example/in"})
+	if err != nil {
+		t.Fatalf("error loading packages: %s", err)
+	}
+
+	msgs, _ := getProtobufTypes(pkgs, "")
+
+	var pkgForTest *message
+	for _, v := range msgs {
+		log.Println(v.Name)
+		if strings.Trim(v.Name, " ") == "EventFieldItem" {
+			pkgForTest = v
+			break
+		}
+	}
+
+	assert.NotNil(t, pkgForTest, "Could not find test struct EventFieldItem")
+
+	for _, v := range pkgForTest.Fields {
+		assert.NotEqual(t, v.Name, "ignore_this_field", "IgnoreThisField should be ignored from parsing")
 	}
 }
